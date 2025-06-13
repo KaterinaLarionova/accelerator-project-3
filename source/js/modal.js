b528d00787e185a4e0fdfc73e5650fc9284ddde5
KaterinaLarionova <lovadza@mail.ru>
2025-06-13 21:42:57 +0300

    add modal.js

--- source/js/modal.js
+++ source/js/modal.js
@@ -1,3 +1,25 @@
+const showModalButton = document.querySelector('.about__button');
+const popupBox = document.querySelector('.popup');
 
+showModalButton.addEventListener('click', (evt) => {
+  showModal();
+  evt.stopPropagation();
+});
 
+function showModal() {
+  popupBox.classList.add('popup__opened');
+  popupBox.classList.remove('popup__closed');
+  document.body.style.overflow = 'hidden';
+}
 
+function hideModal() {
+  popupBox.classList.remove('popup__opened');
+  popupBox.classList.add('popup__closed');
+  document.body.style.overflow = 'auto';
+}
+
+popupBox.addEventListener('click', (evt) => {
+  if (!evt.target.closest('.popup__container') || evt.target.closest('.popup__closed-button')) {
+    hideModal();
+  }
+});
